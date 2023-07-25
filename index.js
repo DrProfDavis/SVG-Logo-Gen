0e4de691708b8304264f4dd0e139943d667d85a7
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-07-25 14:10:12 -0500

    Added psuedo code to index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+// Import Inquirer, fs, and shapes
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// writeToFile function that takes the input from questions to generate svg file
+function writeToFile(answers){
+    // Text
+
+    // Text Color
+
+    // Shape
+
+    // Shape Color
+}
+
+// questions function that asks for input from the user
+function questions() {
+    // Text
+
+    // Text Color
+
+    // Shape
+
+    // Shape Color
+}
+
+
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for text
+// THEN I can enter up to three characters
+// WHEN I am prompted for the text color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I am prompted for a shape
+// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+// WHEN I am prompted for the shape's color
+// THEN I can enter a color keyword (OR a hexadecimal number)
+// WHEN I have entered input for all the prompts
+// THEN an SVG file is created named `logo.svg`
+// AND the output text "Generated logo.svg" is printed in the command line
+// WHEN I open the `logo.svg` file in a browser
+// THEN I am shown a 300x200 pixel image that matches the criteria I entered
