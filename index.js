d3dc17bba766df042f071345fd5087d11ba6feae
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-07-27 15:44:24 -0500

    Working on index

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Import Inquirer, fs, and shapes
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Triangle, Square, Circle} = require("./lib/shapes");
 
-// writeToFile function that takes the input from questions to generate svg file
+// writeToFile function that takes the input from questions and calls shapes.js
 function writeToFile(answers) {
+    // Size
+    let snippet = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">'
     // Text
 
     // Text Color
@@ -55,7 +58,7 @@ function questions() {
         });
 }
 
-
+questions();
 
 
 
