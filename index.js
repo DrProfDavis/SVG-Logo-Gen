c90711f28f796366c5914c056201408dab000167
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-07-27 18:20:19 -0500

    recreated shapes.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function writeToFile(answers) {
     let logoName = `${answers.shapeColor}_${answers.shape}_${answers.text}_logo`
     console.log(logoName);
 
+    // Create SVG file
     fs.writeFile(`./examples/${logoName}.svg`, snippet, (err) => {
         err ? console.log(err) : console.log(`Generated ${logoName}.svg`);
     });
@@ -63,28 +64,11 @@ function questions() {
             if (answers.text.length > 3) {
                 console.log("Please have no more than 3 characters");
                 questions();
-                // TODO Add Another error handling for colors
             } else {
                 writeToFile(answers);
             }
         });
 }
 
+// Initiate
 questions();
-
-
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
