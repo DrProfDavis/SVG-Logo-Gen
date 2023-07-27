738124a53e5c79c6871bbe3a7ba33def9919cee6
Shawn Davis <125922409+DrProfDavis@users.noreply.github.com>
2023-07-27 16:13:22 -0500

    Most logic in index.js done

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 // Import Inquirer, fs, and shapes
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Triangle, Square, Circle} = require("./lib/shapes");
+const { Triangle, Square, Circle } = require("./lib/shapes");
+var width = 300;
+var height = 200;
 
 // writeToFile function that takes the input from questions and calls shapes.js
 function writeToFile(answers) {
-    // Size
-    let snippet = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">'
-    // Text
+    // Size snippet
+    let snippet = `<svg version="1.1" width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg"><g>`
 
-    // Text Color
+    // Shape and Shape Color logic
+    // if (response.shape === 'Circle') {
+    //     let userShape = new shape (response.shapeColor)
+    //     return userShape.render()
+    // }
 
-    // Shape
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        snippet += `${answers.shape}<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        snippet += `${answers.shape}<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        snippet += `${answers.shape}<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;
+    }
 
-    // Shape Color
+    console.log(snippet)
 }
 
 // questions function that asks for input from the user
